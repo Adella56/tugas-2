30a760c5e4993741fd3e6613e0d9fd7860e9610a
Adella56 <43035306+Adella56@users.noreply.github.com>
2018-09-18 15:56:41 +0700

    Add files via upload

---
+++ TT3E2018.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tt3e2018;
+
+/**
+ *
+ * @author ACER
+ */
+public class TT3E2018 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     
+//     //   int x=5;
+//        for (int i=1;i<=5;i++){
+//            for(int j=1;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//    }
+    
+            
+//          //  int nilai= 80;
+//            
+//             if (nilai >= 85);{
+//                 System.out.println("A");
+//                 }
+//            
+//             if (nilai >= 75){
+//                 System.out.println("B");
+//                 }
+//             
+//             else if (nilai >= 65){
+//                  System.out.println("C");
+//                  }
+//                  
+//             else if(nilai >=55){
+//                  System.out.println("D");
+//                   }
+//             else {
+//                 System.out.println("E");  {   
+//    }
+//                     
+//             
+//             }
+//             }
+//}
+
+     
+             
+  
+     int n=5;    
+     for( int i=0 ;i<5;i++){
+     for( int j=0 ;j<5;j++){
+           
+             if (i == 0||i==4 ||j== 0||j == 4){
+                System.out.print("#");
+                }
+             else{
+                 System.out.print("*");
+                 }   
+    }
+  System.out.println();
+    }
+    }
+}
